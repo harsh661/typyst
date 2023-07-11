f7e48f65d95ebf2d370fd412db7296259342a09e
harsh661 <plasmaharsh@gmail.com>
2023-07-11 18:02:20 +0530

    initialted react-router for login route

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,24 @@
 import { LuGithub } from "react-icons/lu"
 import { BiUser } from "react-icons/bi"
 import { FaQuoteLeft } from "react-icons/fa"
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
     <div className="nav_container">
-      <p className="logo">
+      <Link to={'/'} className="logo">
         <span className="logo_icon">
           <FaQuoteLeft />
         </span>
         typyst
-      </p>
+      </Link>
       <div className="icons">
         <a href="https://github.com/harsh661/typyst">
           <LuGithub />
         </a>
-        <a>
+        <Link to={'/login'}>
           <BiUser />
-        </a>
+        </Link>
       </div>
     </div>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,38 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import Navbar from './components/Navbar.tsx'
+import { Outlet } from 'react-router'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 const Layout = () => {
   return (
     <>
       <Navbar />
-      <App />
+      <Outlet />
     </>
   )
 }
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <App />
+      },
+      {
+        path: "/login",
+        element: <h2>Login</h2>
+      },
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Layout />
-  </React.StrictMode>,
+  <RouterProvider router={router}/>
 )
