eed18f94e9c9d9e7c13c1cac9c622af253938077
harsh661 <plasmaharsh@gmail.com>
2023-07-11 15:07:09 +0530

    ci: render time setting in a better way

--- src/components/Setting.tsx
+++ src/components/Setting.tsx
@@ -10,42 +10,25 @@ const Setting: React.FC<SettingProps> = ({ time, setTime }) => {
     setTime(e.target.value)
   }
 
+  const times = [15, 30, 60, 120]
+
   return (
     <div className="settings">
-      <i className="fa fa-clock-o" style={{'fontSize': '24px'}}></i>
-      <label htmlFor="15" className={`btn ${time == 15 && "selected"}`}>
-        15
-        <input
-          className="hidden"
-          value={15}
-          onChange={handleChange}
-          id="15"
-          type="radio"
-          name="time"
-        />
-      </label>
-      <label htmlFor="30" className={`btn ${time == 30 && "selected"}`}>
-        30
-        <input
-          className="hidden"
-          value={30}
-          onChange={handleChange}
-          id="30"
-          type="radio"
-          name="time"
-        />
-      </label>
-      <label htmlFor="60" className={`btn ${time == 60 && "selected"}`}>
-        60
-        <input
-          className="hidden"
-          value={60}
-          onChange={handleChange}
-          id="60"
-          type="radio"
-          name="time"
-        />
-      </label>
+      <i className="fa fa-clock-o" style={{ fontSize: "24px" }}></i>
+
+      {times.map((value) => (
+        <label htmlFor={value.toString()} className={`btn ${time == value && "selected"}`}>
+          {value}
+          <input
+            className="hidden"
+            value={value}
+            onChange={handleChange}
+            id={value.toString()}
+            type="radio"
+            name="time"
+          />
+        </label>
+      ))}
     </div>
   )
 }
