dfde80fe1b753202edb0291e2d60ac86611bd3e5
harsh661 <plasmaharsh@gmail.com>
2023-07-13 14:57:29 +0530

    feat: registered user can login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,19 +1,51 @@
+import { FormEvent, useState } from "react"
 import { CgLogIn, CgGoogle } from "react-icons/cg"
+import { auth } from "../../backend/firebase"
+import { signInWithEmailAndPassword } from "firebase/auth"
+import { useNavigate } from "react-router-dom"
 
 const Login = () => {
+  const navigate = useNavigate()
+  const [email, setEmail] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredentials) => {
+        navigate('/')
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+  
   return (
-    <div className="form signin">
-      <input type="email" placeholder="email" />
-      <input type="password" placeholder="password" />
+    <form onSubmit={handleSubmit} className="form signin">
+      <input
+        type="email"
+        placeholder="email"
+        value={email}
+        required
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        required
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button>
         <CgLogIn />
         Login
       </button>
-      <button>
+      <div>
         <CgGoogle />
         Google
-      </button>
-    </div>
+      </div>
+    </form>
   )
 }
 
